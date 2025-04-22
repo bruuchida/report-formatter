01e4cfff7af9df3dd276636e4cdfc6539b4a01c3
bruuchida <tiemiuchida@hotmail.com.br>
2025-04-22 09:19:10 -0300

    feat: add other custom project option

--- js/script.js
+++ js/script.js
@@ -1,44 +1,37 @@
-const date = new Date();
+const date = new Date()
 
 window.onload = () => {
-    initTheme();
-    getTodayDate();
-    document.getElementById('task-description').placeholder = 'Um item por linha';
-    document.querySelector("#project").addEventListener("input", () => {
-        let project = document.querySelector("#project").value;
-        if (project === "fm-site-br") {
-            document.querySelector("#deploy-time-field").style.display = "grid";
-            document.querySelector("#deploy-time-anytime-field").style.display = "none";
-        } else {
-            document.querySelector("#deploy-time-field").style.display = "none";
-            document.querySelector("#deploy-time-anytime-field").style.display = "grid";
-        }
-    })
+    initTheme()
+    getTodayDate()
+    document.getElementById('task-description').placeholder = 'Um item por linha'
+    checkDeployTime()
+    checkCustomProject()
 }
 
 function getReport() {
-    let report = "";
-    document.querySelector("#report-formatted").innerHTML = report;
-    let squad = document.querySelector("#squad").value;
-    let date = document.querySelector("#date").value;
-    let project = document.querySelector("#project").value;
-    
-    // let deployTimeInitial = document.querySelector("#deploy-time-initial").value;
-    // let deployTimeFinal = document.querySelector("#deploy-time-final").value;
+    let report = ""
+    document.querySelector("#report-formatted").innerHTML = report
+    let squad = document.querySelector("#squad").value
+    let date = document.querySelector("#date").value
+    let project
+
+    if (document.querySelector("#project").value === 'outro') {
+        project = document.querySelector('#project-custom').value
+    }
 
     let deployTime
 
     if (project === 'fm-site-br') {
-        deployTime = document.querySelector("#deploy-time").value;
+        deployTime = document.querySelector("#deploy-time").value
     } else {
-        deployTime = document.querySelector("#deploy-time-anytime").value;
+        deployTime = document.querySelector("#deploy-time-anytime").value
     }
 
-    let feature = document.querySelector("#feature").value;
-    let taskName = document.querySelector("#task-name").value;
-    let taskLink = document.querySelector("#task-link").value;
-    let taskDescription = getListFormat(document.querySelector("#task-description").value);
-    let taskArea = document.querySelector("#task-area").value;
+    let feature = document.querySelector("#feature").value
+    let taskName = document.querySelector("#task-name").value
+    let taskLink = document.querySelector("#task-link").value
+    let taskDescription = getListFormat(document.querySelector("#task-description").value)
+    let taskArea = document.querySelector("#task-area").value
 
     report = `
         <b>Time ${squad}</b><br>
@@ -47,6 +40,7 @@ function getReport() {
         <b>Hora: ${deployTime}</b><br>
         <b>Épico: </b>${feature}<br>
         <b>Nome da tarefa e link: </b><a href='${taskLink}'>${taskName}</a><br>
+        <hr/>
     `;
 
     if (taskDescription) {
@@ -59,53 +53,67 @@ function getReport() {
     if (taskArea) {
         report += `
             <b>Área: </b>${taskArea}<br></br>
-        `;
+        `
     }    
 
-    document.querySelector("#report-formatted").innerHTML = report;
-    document.querySelector("#report-content").innerHTML = report;
+    document.querySelector("#report-formatted").innerHTML = report
+    document.querySelector("#report-content").innerHTML = report
 }
 
 function visualizeReport() {
     getReport();
-    document.querySelector('#form').style.display = "none";
-    document.querySelector('#squad-field').style.display = "none";
-    document.querySelector('#result').style.display = "block";
+    document.querySelector('#form').style.display = "none"
+    document.querySelector('#squad-field').style.display = "none"
+    document.querySelector('#result').style.display = "block"
 }
 
 function copyContent() {
     const range = document.createRange();
-    range.selectNode(document.querySelector("#report-content"));
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();
+    range.selectNode(document.querySelector("#report-content"))
+    window.getSelection().removeAllRanges()
+    window.getSelection().addRange(range)
+    document.execCommand("copy")
+    window.getSelection().removeAllRanges()
 
-    // document.querySelector('#copied-message').classList.add('show');
-    // setTimeout(()=> {
-    //     document.querySelector('#copied-message').classList.remove('show');
-    // }, 2000);
+    document.querySelector('#copied-message').classList.add('show')
+    setTimeout(()=> {
+        document.querySelector('#copied-message').classList.remove('show')
+    }, 2000)
 }
 
 function getReportAndCopy() {
-    getReport();
-    copyContent();
+    getReport()
+    copyContent()
 }
 
 function checkDeployTime() {
-   
-   
+    document.querySelector("#project").addEventListener("input", () => {
+        let project = document.querySelector("#project").value
+        if (project === "fm-site-br") {
+            document.querySelector("#deploy-time-field").classList.remove("invisible")
+            document.querySelector("#deploy-time-anytime-field").classList.add("invisible")
+        } else {
+            document.querySelector("#deploy-time-field").classList.add("invisible")
+            document.querySelector("#deploy-time-anytime-field").classList.remove("invisible")
+
+            if (project === "outro") {
+                document.querySelector("#project-custom-field").classList.remove("invisible")
+            } else {
+                document.querySelector("#project-custom-field").classList.add("invisible")
+            }
+        }
+    })
 }
 
 /* Theme */
 function initTheme() {
     const theme = localStorage.getItem('selected-theme')
-    setTheme(theme !== null ? theme : 'theme-light');
+    setTheme(theme !== null ? theme : 'theme-light')
 }
 
 function setTheme(theme) {
     localStorage.setItem('selected-theme', theme);
-    document.body.classList = theme;
+    document.body.classList = theme
     document.querySelector('#theme').src = `./icons/${theme}.svg`
 }
 
@@ -119,18 +127,18 @@ function toggleTheme() {
 }
 
 function backToReport() {
-    document.querySelector('#squad-field').style.display = "flex";
-    document.querySelector('#form').style.display = "grid";
-    document.querySelector('#result').style.display = "none";
+    document.querySelector('#squad-field').style.display = "flex"
+    document.querySelector('#form').style.display = "grid"
+    document.querySelector('#result').style.display = "none"
 }
 
 /* Formats */
 function getTodayDate() {
-    let temp =  date.getDate().toString().padStart(2, 0) + '/' + (date.getMonth() + 1).toString().padStart(2, 0) + '/' + date.getFullYear();
-    document.querySelector('#date').value = temp;
+    let temp =  date.getDate().toString().padStart(2, 0) + '/' + (date.getMonth() + 1).toString().padStart(2, 0) + '/' + date.getFullYear()
+    document.querySelector('#date').value = temp
 }
 
 function getListFormat(text) {
-    var lines = text.split('\n');
-    return lines.filter(line => line.trim() !== '').map(line => `<li>${line}</li>`).join('');
+    var lines = text.split('\n')
+    return lines.filter(line => line.trim() !== '').map(line => `<li>${line}</li>`).join('')
 }
